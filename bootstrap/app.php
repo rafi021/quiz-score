cad3719588b729bd84d10f20fe881e305ad03d25
rafi021 <mahmud.ibrahim021@gmail.com>
2024-06-18 15:26:13 +0600

    add db

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->statefulApi();
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
